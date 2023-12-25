fa4b231304d80ef29583c064fa4e66113fef0a32
Natalia Lebedeva <nataliadaveng@gmail.com>
2023-12-25 18:45:58 +0600

    feat: create project config

---
+++ vue-project/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <main class="w-4/5 h-full m-auto my-10 bg-white rounded-xl shadow-xl">
+    <h1>Vue Title</h1>
+    <HelloWorld title="Title" :price="5">
+      <h1>Added title</h1>
+    </HelloWorld>
+  </main>
+</template>
+
+<style scoped></style>
---
+++ vue-project/src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<!-- <script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    title: String,
+    price: Number
+  },
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    }
+  }
+})
+</script> -->
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps({
+  title: String,
+  price: Number
+})
+
+let count = ref(0)
+
+function increment() {
+  count.value++
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{ count }}</h1>
+    <p>{{ title }}</p>
+    <p>{{ price }}</p>
+    <button class="border" @click="increment">Add</button>
+    <input @keyup.ctrl.enter="increment" />
+    <slot></slot>
+  </div>
+</template>
